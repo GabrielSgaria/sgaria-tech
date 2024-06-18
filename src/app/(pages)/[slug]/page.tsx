3349d62976f4bea9c7cccf74ceb1e07e83ffe11a
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-06-17 22:31:01 -0300

    feat: portfolio responsive

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -56,13 +56,13 @@ export default async function PageJob({ params: { slug } }: PageJobProps) {
     return (
 
         <div className={cn(`h-full `, colorBgMap[slug])}>
-            <div className="flex container mx-auto py-40 h-full">
-                <section className="w-full flex flex-col justify-center items-center px-20 gap-10">
+            <div className="flex container mx-auto pt-40 pb-20 h-full">
+                <section className="w-full flex flex-col justify-center items-center px-3 md:px-20 gap-10">
                     <div className="flex flex-col gap-3 items-start w-full pl-5">
-                        <h1 className="text-5xl text-zinc-50 font-bold text-start">{portfolio.title}</h1>
-                        <p className="text-lg text-zinc-200 max-w-[930px]">{portfolio.description}</p>
+                        <h1 className="text-4xl md:text-5xl text-zinc-50 font-bold text-start">{portfolio.title}</h1>
+                        <p className="text-base md:text-lg text-zinc-200 max-w-[930px]">{portfolio.description}</p>
                     </div>
-                    <div className="w-10/12 rounded-lg">
+                    <div className="w-full md:w-10/12 rounded-lg hidden md:flex">
                         <Image 
                             width={1480}
                             height={690}
@@ -72,7 +72,7 @@ export default async function PageJob({ params: { slug } }: PageJobProps) {
                             className="w-full rounded-lg "
                         />
                     </div>
-                    <div className="grid gap-5 grid-cols-1 md:grid-cols-2 w-10/12">
+                    <div className="grid gap-5 grid-cols-1 md:grid-cols-2 w-full md:w-10/12">
                         {portfolio.images.map((item, index) => (
                             <div key={index} className="shadow-xl w-full">
                                 <Image
